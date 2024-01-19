92eed2ba094c5d337913ee745de51e632bf126ac
ferdinands <ferdinand.strobel@infrontfinance.com>
2024-01-19 12:02:58 +0100

    Init

---
+++ src/main/java/com/name_template/NameTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.name_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/name_template/NameTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.name_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
