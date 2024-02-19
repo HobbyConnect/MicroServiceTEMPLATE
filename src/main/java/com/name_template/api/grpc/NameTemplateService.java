71d8e54d8cfc6467381fef4baada3229c9621f30
ferdinand <ferdistr08@gmai.com>
2024-02-19 23:01:49 +0100

    add grpc-server

--- src/main/java/com/name_template/api/grpc/NameTemplateService.java
+++ src/main/java/com/name_template/api/grpc/NameTemplateService.java
@@ -5,7 +5,10 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.examples.lib.HelloReply;
 import net.devh.boot.grpc.examples.lib.HelloRequest;
 import net.devh.boot.grpc.examples.lib.MyServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
 
+
+@GrpcService
 public class NameTemplateService extends MyServiceGrpc.MyServiceImplBase {
 
 
