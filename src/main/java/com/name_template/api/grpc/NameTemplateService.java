2784508763f116afe4acf5e73d756f96499d6b21
ferdinands <ferdinand.strobel@infrontfinance.com>
2024-02-20 10:42:07 +0100

    health

--- src/main/java/com/name_template/api/grpc/NameTemplateService.java
+++ src/main/java/com/name_template/api/grpc/NameTemplateService.java
@@ -1,10 +1,9 @@
 package com.name_template.api.grpc;
 
 
+import com.name_template.name_template.GrpcRequestHandler;
 import io.grpc.stub.StreamObserver;
-import net.devh.boot.grpc.examples.lib.HelloReply;
-import net.devh.boot.grpc.examples.lib.HelloRequest;
-import net.devh.boot.grpc.examples.lib.MyServiceGrpc;
+import net.devh.boot.grpc.examples.lib.*;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 
@@ -12,19 +11,32 @@ import net.devh.boot.grpc.server.service.GrpcService;
 public class NameTemplateService extends MyServiceGrpc.MyServiceImplBase {
 
     /*
-    Doc:
-        https://yidongnan.github.io/grpc-spring-boot-starter/en/server/getting-started.html
+        Implement here all endpoint which added in the NameTemplateService.proto
      */
+
+    private GrpcRequestHandler grpcRequestHandler;
+
+    public NameTemplateService(GrpcRequestHandler grpcRequestHandler){
+        this.grpcRequestHandler = grpcRequestHandler;
+    };
+
 
 
     @Override
-    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        HelloReply reply = HelloReply.newBuilder()
-                .setMessage("Hello ==> " + request.getName())
-                .build();
-        responseObserver.onNext(reply);
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        HelloResponse response = grpcRequestHandler.hello(request);
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
+
+    @Override
+    public void healthCheck(HealthCheckRequest request, StreamObserver<HealthCheckResponse> responseObserver) {
+        HealthCheckResponse response = grpcRequestHandler.health();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+
 
 
 
